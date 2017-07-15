d0adda4fb97bbf889c5782e49a426fc6157b3be2
jaime <garciadiazjaime@gmail.com>
2017-07-15 13:17:15 -0500

    remove unused

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@
 
 const constants = {
   schoolId: '59682d4aae43b7b792500f08',
-  apiUrl: 'http://api.irk.mx/api',
-  docsUrl: 'http://app.irk.mx/docs',
+  apiUrl: 'http://127.0.0.1:3000/api',
+  docsUrl: 'http://127.0.0.1:3000/docs',
   appTitle: 'Kolbe Admin App',
   invalidText: 'Obligatorio',
   emptyData: 'No hay información',
