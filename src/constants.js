b89cb317ebd815a089b7cc079e5e160455ef978e
jaime <garciadiazjaime@gmail.com>
2017-07-14 19:15:19 -0500

    build

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@
 
 const constants = {
   schoolId: '58fbde6f393b1b1bd8536b5a',
-  apiUrl: 'http://127.0.0.1:3000/api',
+  apiUrl: window.apiUrl || 'http://127.0.0.1:3000/api',
   docsUrl: 'http://127.0.0.1:3000/docs',
   appTitle: 'Kolbe Admin App',
   invalidText: 'Obligatorio',
--- src/server/server.jsx
+++ src/server/server.jsx
@@ -26,7 +26,9 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/*', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    apiUrl: config.get('api.url'),
+  });
 });
 
 app.set('ipaddress', config.get('ipaddress'));
